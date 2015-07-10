b4ec2bfc5007a01169b9cbe696683a082cac25ce
Bill <billwuyu@gmail.com>
2015-07-10 06:29:00 -0700

    attempt 17

--- final_project.py
+++ final_project.py
@@ -162,7 +162,7 @@ def upcomingEvents():
 
 
 if __name__ == "__main__":
-	app.secret_key = 'super_secret_key'
+	app.secret_key = 'yet_another_secret_key'
 	#app.debug = True
 	#app.run(host = '0.0.0.0', port = 5000)
 	app.run()
